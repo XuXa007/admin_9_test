0be8c35af486a7cf94167c710fa7ddc2fc1482e7
Ксюша Лягашкина <xl@192.168.1.3>
2024-12-16 23:50:30 +0300

    test

--- src/main/java/com/example/admin_9_test/Admin9TestApplication.java
+++ src/main/java/com/example/admin_9_test/Admin9TestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Admin9TestApplication {
 
     public static void main(String[] args) {
+        System.out.println("Hello, Jenkins");
         SpringApplication.run(Admin9TestApplication.class, args);
     }
 
