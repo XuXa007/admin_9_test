97ed253fe7e41302544d4e05863263a07eb16a43
Ксюша Лягашкина <xl@192.168.1.3>
2024-12-16 23:39:15 +0300

    first commit

---
+++ src/main/java/com/example/admin_9_test/Admin9TestApplication.java
@@ -0,0 +1,13 @@
+package com.example.admin_9_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Admin9TestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Admin9TestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/admin_9_test/Admin9TestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.admin_9_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Admin9TestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
